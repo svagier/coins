2643c769a315532c2be06e85f03a334a205b4d57
Piotr Danielczyk <piotrekdanielczyk@gmail.com>
2019-07-07 12:07:54 +0200

    Improved efficiency by adding dictionary

--- coins.py
+++ coins.py
@@ -1,25 +1,29 @@
-def divide(value):
-    a = value // 2
-    b = value // 3
-    c = value // 4
-    return a, b, c
+saved = {}
 
 def max_return(value):
-    total = 0
-    (a, b, c) = divide(value)
-    if a + b + c > value:
-        total += max_return(a)
-        total += max_return(b)
-        total += max_return(c)
+    if value in saved:
+        return saved[value]
     else:
-        total += value
-    return total
+        total = 0
+        (a, b, c) = (value // 2, value // 3, value // 4)
+        if a + b + c > value:
+            total += max_return(a)
+            total += max_return(b)
+            total += max_return(c)
+        else:
+            total += value
+        saved[value] = total
+        return total
 
 
 def main():
-    for testcase in range(10):
-        coin = int(input())
-        print(max_return(coin))
+
+    while True:
+        try:
+            coin = int(input())
+            print(max_return(coin))
+        except:
+            break
 
 
 if __name__ == "__main__":
